81a0fcdac7681262ff5a96aff26e3856bd85a431
Byuntil <gugugu2014@jbnu.ac.kr>
2021-02-13 22:37:41 +0900

    2020.02.12

--- js/hub.js
+++ js/hub.js
@@ -2,66 +2,24 @@ const tabHub = document.querySelector(".main-nav-btns-right");
 const show = document.querySelector(".main-tab-hub");
 const closeBtn = show.querySelector(".icon-close");
 
-
-function tabClose(){
-    show.style.top='-100%';
+function tabClose() {
+  show.style.top = "-100%";
 }
-function tabHubShow(){
-    show.style.top='3%';
+function tabHubShow() {
+  show.style.top = "3%";
 }
 
 tabHub.addEventListener("click", tabHubShow);
 closeBtn.addEventListener("click", tabClose);
 
-const hubView = document.querySelector(".main-tab-hub-view")
+const hubView = document.querySelector(".main-tab-hub-view");
 
 const bookmarkBtn = document.querySelector("#bookmark");
 const todoBtn = document.querySelector("#todo");
-const noteBtn = document.querySelector("#note");
+const bookmarkView = document.querySelector(".bookmarks-view");
+const toDoView = document.querySelector(".todos-view");
 
-
-function appendHtml(loaded, num){
-    let div = document.createElement('div');
-    if(num===1){
-    div.className="bookmarks-view";
-    }else if(num===2){
-        div.className="todos-view";
-    }else if(num===3){
-        div.className="notes-view";
-    }else{
-
-    }
-    div.innerHTML=loaded;
-    hubView.appendChild(div);
-}
-function bookmarkShow(){
-    hubView.innerHTML="";
-    let load = `<div class="bookmark-box"><div class="bookmark"><div class="bookmark-head"><img class="bookmark-favicon" src = "http://www.google.com/s2/favicons?domain=http://google.com"></img><div class="bookmark-title">Google</div><div class="bookmark-setting-btn"><i class="fas fa-ellipsis-h"></i></div></div><a href="http://google.com" class="bookmark-body" style="background-image:url('img/google.PNG')"></a></div></div>
-    <div class="bookmark-box"><div class="bookmark"><div class="bookmark-head"><img class="bookmark-favicon" src = "http://www.google.com/s2/favicons?domain=http://naver.com"></img><div class="bookmark-title">Naver</div><div class="bookmark-setting-btn"><i class="fas fa-ellipsis-h"></i></div></div><a href="http://naver.com" class="bookmark-body" style="background-image:url('img/naver.PNG')"></a></div></div>
-    <div class="bookmark-box"><div class="bookmark"><div class="bookmark-head"><img class="bookmark-favicon" src = "http://www.google.com/s2/favicons?domain=http://nomadcoders.co/"></img><div class="bookmark-title">Nomardcoders</div><div class="bookmark-setting-btn"><i class="fas fa-ellipsis-h"></i></div></div><a href="http://nomadcoders.co/" class="bookmark-body" style="background-image:url('img/nomadcoders.PNG')"></a></div></div>
-    <div class="bookmark-box"><div class="bookmark"><div class="bookmark-head"><i class="fab fa-github bookmark-favicon"></i><div class="bookmark-title">Github</div><div class="bookmark-setting-btn"><i class="fas fa-ellipsis-h"></i></div></div><a href="https://github.com/" class="bookmark-body" style="background-image:url('img/github.PNG')"></a></div></div>
-    <div class="bookmark-box"><div class="bookmark"><div class="bookmark-head"><img class="bookmark-favicon" src = "http://www.google.com/s2/favicons?domain=http://stackoverflow.com"></img><div class="bookmark-title">Stackoverflow.com</div><div class="bookmark-setting-btn"><i class="fas fa-ellipsis-h"></i></div></div><a href="http://stackoverflow.com" class="bookmark-body" style="background-image:url('img/stackoverflow.PNG')"></a></div></div>
-    <div class="bookmark-box"><div class="bookmark"><div class="bookmark-head"><img class="bookmark-favicon" src = "http://www.google.com/s2/favicons?domain=http://www.reddit.com"></img><div class="bookmark-title">Reddit</div><div class="bookmark-setting-btn"><i class="fas fa-ellipsis-h"></i></div></div><a href="http://www.reddit.com" class="bookmark-body" style="background-image:url('img/reddit.PNG')"></a></div></div>
-    <div class="bookmark-box"><div class="bookmark"><div class="bookmark-head"><img class="bookmark-favicon" src = "http://www.google.com/s2/favicons?domain=http://instagram.com"></img><div class="bookmark-title">Instagram</div><div class="bookmark-setting-btn"><i class="fas fa-ellipsis-h"></i></div></div><a href="http://instagram.com" class="bookmark-body" style="background-image:url('img/instagram.PNG')"></a></div></div>
-    <div class="bookmark-box"><div class="bookmark"><div class="bookmark-head"><img class="bookmark-favicon" src = "http://www.google.com/s2/favicons?domain=http://www.youtube.com"></img><div class="bookmark-title">Youtube</div><div class="bookmark-setting-btn"><i class="fas fa-ellipsis-h"></i></div></div><a href="http://www.youtube.com" class="bookmark-body" style="background-image:url('img/youtube.PNG')"></a></div></div>
-    <div class="bookmark-box"><div class="bookmark"><div class="bookmark-head"><img class="bookmark-favicon" src = "http://www.google.com/s2/favicons?domain=https://developer.mozilla.org/"></img><div class="bookmark-title">Mdn</div><div class="bookmark-setting-btn"><i class="fas fa-ellipsis-h"></i></div></div><a href="https://developer.mozilla.org/" class="bookmark-body" style="background-image:url('img/mdn.PNG')"></a></div></div>
-    <div class="bookmark-box"><div class="bookmark"><i class="fas fa-plus"></i></div></div>
-    <div class="bookmark-box"><div class="bookmark"><i class="fas fa-plus"></i></div></div>
-    <div class="bookmark-box"><div class="bookmark"><i class="fas fa-plus"></i></div></div>
-    <div class="bookmark-box"><div class="bookmark"><i class="fas fa-plus"></i></div></div>
-    <div class="bookmark-box"><div class="bookmark"><i class="fas fa-plus"></i></div></div>
-    <div class="bookmark-box"><div class="bookmark"><i class="fas fa-plus"></i></div></div>
-    <div class="bookmark-box"><div class="bookmark"><i class="fas fa-plus"></i></div></div>`;
-    appendHtml(load, 1);
-}
-
-function todoShow(){
-    hubView.innerHTML="";
-    let load = `<div class="left-part"><div class="wt-todolist-view"><div class="wt-list-view-header">
-    </div><div></div></div><button class="add-btn">Add</button></div>
-     <div class=right-part></div>`;
-     appendHtml(load,2);
-}
+/*
 function saveNote(){
         const inputtext = document.querySelector("#text");
         const texts = inputtext.value;
@@ -72,7 +30,7 @@ function noteShow(){
     let load = `<div class="left-part"><div class="notes-box"><div class="note a"><div class="note-title-box">
                 <div class="note-title">New Note</div><div class="note-time"><span></span></div></div></div></div>
                 <button class="add-btn">Add</button></div><div class="right-part"><div class="note-time"><span></span>
-                </div><textarea id="text"></textarea><button class="save-btn">save</button>`;
+                </div><textarea id="text">sdd</textarea><button class="save-btn">save</button>`;
     appendHtml(load, 3);
     const l = document.querySelector(".left-part");
     const n = l.querySelector(".note-time");
@@ -84,9 +42,19 @@ function noteShow(){
     d_.innerText = `${getDate()}`;
     document.querySelector(".save-btn").addEventListener("click", saveNote);
 }
+*/
+function bookmarkShow() {
+  toDoView.classList.remove("aT");
+  bookmarkView.classList.add("aB");
+}
+function todoShow() {
+  bookmarkView.classList.remove("aB");
+  toDoView.classList.add("aT");
+}
 bookmarkBtn.addEventListener("click", bookmarkShow);
 todoBtn.addEventListener("click", todoShow);
-noteBtn.addEventListener("click", noteShow);
+/*noteBtn.addEventListener("click", noteShow);
+
 const addBtn = document.querySelector(".add-btn");
 function addNote(){
     const textarea = document.createElement('textarea');
@@ -111,3 +79,4 @@ function getDate(){
     var t = `${weekday[date.getDay()]} ${dat<10?`0${dat}`: dat}, ${year} ${hours<10?`0${hours}`:hours}:${minuties<10?`0${minuties}`:minuties}`;
     return t;
 }
+*/
